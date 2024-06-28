e4c850430ad13fbe1203540bb461bfeb9dfde713
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-06-29 01:28:33 +0300

    Create TransactionBar.tsx

---
+++ src/components/TransactionBar.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Box, Flex, Input, Select, Button, useBreakpointValue, InputGroup, InputLeftElement, } from '@chakra-ui/react';
+import { MdOutlineSearch } from "react-icons/md";
+
+interface TransactionBarProps {
+  searchQuery: string;
+  setSearchQuery: (query: string) => void;
+  filters: { status: string; type: string };
+  setFilters: (filters: { status: string; type: string }) => void;
+  onImportClick: () => void;
+  onExportClick: () => void;
+}
+
+const TransactionBar: React.FC<TransactionBarProps> = ({ searchQuery, setSearchQuery, filters, setFilters, onImportClick, onExportClick }) => {
+  const inputSize = useBreakpointValue({ base: 'sm', md: 'md' });
+
+  return (
+    <Flex justify="space-between" wrap="wrap" mb={4} align="center">
+    <Box>
+       <Flex flex="1" justify="space-between" align="center">
+        <InputGroup>
+    <InputLeftElement pointerEvents='none'>
+      <MdOutlineSearch color='teal' size={24}/>
+    </InputLeftElement>
+    <Input
+                          placeholder="Search Clients"
+                          color='teal'
+                          _placeholder={{ color: 'inherit' }}
+                          variant='filled'
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          size={inputSize}
+          w="240px"
+          mr={2}
+        />
+  </InputGroup>
+
+        <Select
+          placeholder="Status"
+          value={filters.status}
+          onChange={(e) => setFilters({ ...filters, status: e.target.value })}
+          size={inputSize}
+          maxW="140px"
+                      mr={2}
+                      borderColor='teal'
+                      color='teal'
+                       _placeholder={{ color: 'inherit' }}
+        >
+          <option value="Pending">Pending</option>
+          <option value="Completed">Completed</option>
+          <option value="Cancelled">Cancelled</option>
+        </Select>
+        <Select
+          placeholder="Type"
+          value={filters.type}
+          onChange={(e) => setFilters({ ...filters, type: e.target.value })}
+          size={inputSize}
+                      maxW="140px"
+                      borderColor='teal'
+                      color='teal'
+                       _placeholder={{ color: 'inherit' }}
+        >
+          <option value="Refill">Refill</option>
+          <option value="Withdrawal">Withdrawal</option>
+        </Select>
+        </Flex>
+</Box>
+       <Box>
+        <Button size={inputSize} onClick={onImportClick} mr={2} colorScheme='teal' variant='outline'>
+          Import
+        </Button>
+        <Button size={inputSize} onClick={onExportClick} colorScheme='teal' variant='outline'>
+          Export
+        </Button>
+      </Box>
+    </Flex>
+  );
+};
+
+export default TransactionBar;
