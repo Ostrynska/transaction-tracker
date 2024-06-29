656a86ccc3fee62f97da60424f5a3897cae42e3b
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-06-29 18:16:31 +0300

    Update chakra.ts

--- src/styles/chakra.ts
+++ src/styles/chakra.ts
@@ -1,4 +1,3 @@
-import { Height } from './../../node_modules/csstype/index.d';
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
@@ -6,35 +5,16 @@ const theme = extendTheme({
     heading: "'Poppins', sans-serif",
     body: "'Poppins', sans-serif",
   },
-  colors: {
-    brand: {
-      50: '#e3f8ff',
-      100: '#b3ecff',
-      200: '#81defd',
-      300: '#5ed0fa',
-      400: '#40c3f7',
-      500: '#2bb0ed',
-      600: '#1992d4',
-      700: '#127fbf',
-      800: '#0b69a3',
-      900: '#035388',
-    },
-  },
   styles: {
     global: {
       'html, body': {
         color: 'gray.600',
         lineHeight: 'tall',
         minHeight: '100vh',
-        // display: 'flex',
-        // flexWrap: 'wrap',
-        // alignContent: 'space-between',
-        // justifyContent: 'center',
-        // padding: '0 0 60px'
       },
       a: {
-        color: 'teal.500',
-          },
+        textDecoration: 'none !important',
+      }
     },
   },
 });
