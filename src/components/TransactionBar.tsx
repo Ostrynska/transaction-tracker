71652d79d63558a871db2d2c95c2ebd0a33f79c1
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-06-29 22:23:57 +0300

    Update TransactionBar.tsx

--- src/components/TransactionBar.tsx
+++ src/components/TransactionBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Box, Flex, Input, Select, Button, useBreakpointValue, InputGroup, InputLeftElement, } from '@chakra-ui/react';
+import { Box, Flex, Input, Select, Button, useBreakpointValue, InputGroup, InputLeftElement, IconButton } from '@chakra-ui/react';
 import { MdOutlineSearch } from "react-icons/md";
+import { LuRefreshCcw } from "react-icons/lu";
 
 interface TransactionBarProps {
   searchQuery: string;
@@ -14,58 +15,72 @@ interface TransactionBarProps {
 const TransactionBar: React.FC<TransactionBarProps> = ({ searchQuery, setSearchQuery, filters, setFilters, onImportClick, onExportClick }) => {
   const inputSize = useBreakpointValue({ base: 'sm', md: 'md' });
 
+  const handleRefresh = () => {
+    setSearchQuery('');
+    setFilters({ status: '', type: '' });
+  };
+
   return (
     <Flex justify="space-between" wrap="wrap" mb='48px' align="center">
-    <Box>
-       <Flex flex="1" justify="space-between" align="center">
-        <InputGroup>
-    <InputLeftElement pointerEvents='none'>
-      <MdOutlineSearch color='teal' size={24}/>
-    </InputLeftElement>
-    <Input
-                          placeholder="Search Clients"
-                          color='teal'
-                          _placeholder={{ color: 'inherit' }}
-                          variant='filled'
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          size={inputSize}
-          w="240px"
-          mr={2}
-        />
-  </InputGroup>
+      <Box>
+        <Flex flex="1" justify="space-between" align="center">
+          <InputGroup>
+            <InputLeftElement pointerEvents='none'>
+              <MdOutlineSearch color='teal' size={24}/>
+            </InputLeftElement>
+            <Input
+              placeholder="Search Clients"
+              color='teal'
+              _placeholder={{ color: 'inherit' }}
+              variant='filled'
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              size={inputSize}
+              w="240px"
+              mr={2}
+            />
+          </InputGroup>
 
-        <Select
-          placeholder="Status"
-          value={filters.status}
-          onChange={(e) => setFilters({ ...filters, status: e.target.value })}
-          size={inputSize}
-          maxW="140px"
-                      mr={2}
-                      borderColor='teal'
-                      color='teal'
-                       _placeholder={{ color: 'inherit' }}
-        >
-          <option value="Pending">Pending</option>
-          <option value="Completed">Completed</option>
-          <option value="Cancelled">Cancelled</option>
-        </Select>
-        <Select
-          placeholder="Type"
-          value={filters.type}
-          onChange={(e) => setFilters({ ...filters, type: e.target.value })}
-          size={inputSize}
-                      maxW="140px"
-                      borderColor='teal'
-                      color='teal'
-                       _placeholder={{ color: 'inherit' }}
-        >
-          <option value="Refill">Refill</option>
-          <option value="Withdrawal">Withdrawal</option>
-        </Select>
+          <Select
+            placeholder="Status"
+            value={filters.status}
+            onChange={(e) => setFilters({ ...filters, status: e.target.value })}
+            size={inputSize}
+            maxW="140px"
+            mr={2}
+            borderColor='teal'
+            color='teal'
+            _placeholder={{ color: 'inherit' }}
+          >
+            <option value="Pending">Pending</option>
+            <option value="Completed">Completed</option>
+            <option value="Cancelled">Cancelled</option>
+          </Select>
+          <Select
+            placeholder="Type"
+            value={filters.type}
+            onChange={(e) => setFilters({ ...filters, type: e.target.value })}
+            size={inputSize}
+            maxW="140px"
+            borderColor='teal'
+            color='teal'
+            _placeholder={{ color: 'inherit' }}
+          >
+            <option value="Refill">Refill</option>
+            <option value="Withdrawal">Withdrawal</option>
+          </Select>
+          <IconButton
+            icon={<LuRefreshCcw />}
+            size={inputSize}
+            onClick={handleRefresh}
+            aria-label="Refresh"
+            colorScheme='teal'
+            variant='outline'
+            ml={2}
+          />
         </Flex>
-</Box>
-       <Box>
+      </Box>
+      <Box>
         <Button size={inputSize} onClick={onImportClick} mr={2} colorScheme='teal' variant='outline'>
           Import
         </Button>
