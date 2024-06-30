1504be94c940ac4ffd4e8341f5bad77046f8d28f
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-06-30 15:58:08 +0300

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Text, VStack } from '@chakra-ui/react';
 
 const NotFound: React.FC = () => {
-  return <div>404 - Page Not Found</div>;
+  return <VStack spacing={4} mt={4}><Text fontSize="lg" color="gray.500">404 - Page Not Found</Text></VStack>;
 };
 
 export default NotFound;
