b5972dbc7dd7a198cd533439aa0e2e9b80e4adbf
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-06-30 19:42:57 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
 
 import App from './components/App.tsx'
 import theme from './styles/chakra';
@@ -14,18 +13,11 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0Provider
-        domain="dev-mbuxzrhofy1nn4wo.eu.auth0.com"
-        clientId="4UotCyLuwpBEDRYBdAidXgZJUAPLjzrI"
-        authorizationParams={{
-          redirect_uri: window.location.origin
-        }}>
-        <QueryClientProvider client={queryClient}>
-          <ChakraProvider theme={theme}>
-            <App />
-          </ChakraProvider>
-        </QueryClientProvider>
-      </Auth0Provider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
